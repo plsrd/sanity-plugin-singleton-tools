b03166b6c7803f15ab5c3c696be08c85be04627f
RD <racheal.larimer@gmail.com>
2024-03-05 14:20:11 -0800

    reconfigure document list item args

--- src/structure/index.ts
+++ src/structure/index.ts
@@ -1,35 +1,58 @@
 import { getSingletonDocuments } from '../helpers';
 import { DocumentIcon } from '@sanity/icons';
 import { StructureBuilder, StructureContext } from 'sanity/structure';
-import { SingletonDocumentListItemConfig } from '../types';
+import {
+  SingletonDocumentListItemConfig,
+  SingletonPluginListItemsConfig,
+} from '../types';
 
 const singletonDocumentListItem = (config: SingletonDocumentListItemConfig) => {
-  const { S, schemaType, title, icon, id } = config;
-  const listTitle = title ?? schemaType;
+  if (!config?.S || !config?.type || !config.context) {
+    throw new Error(`
+      S, context, and type must be provided to your singletonDocumentListItem
+      Ex: singletonDocumentListItem({ S, context, type: 'product'})
+    `);
+  }
+  const { S, type, title, icon, id, context } = config;
+  const { schema } = context;
+  const listTitle = title ?? schema.get(type)?.title ?? type;
   const listIcon = icon ?? DocumentIcon;
-  const listId = id ?? schemaType;
+  const listId = id ?? type;
 
   return S.listItem()
     .title(listTitle)
     .icon(listIcon)
-    .child(S.document().schemaType(schemaType).id(listId));
+    .child(S.document().schemaType(type).title(listTitle).id(listId));
 };
 
-const singletonDocumentListItems = (
-  S: StructureBuilder,
-  { schema }: StructureContext
-) => {
+const singletonDocumentListItems = (config: SingletonPluginListItemsConfig) => {
+  if (!config.S || !config.context) {
+    throw new Error(`
+      S and context must be provided
+      Ex: singletonDocumentListItems({ S, context })
+    `);
+  }
+
+  const { S, context } = config;
+  const { schema } = context;
+
   const singletons = getSingletonDocuments(schema);
 
   return singletons?.map(schemaType =>
-    singletonDocumentListItem({ S, schemaType })
+    singletonDocumentListItem({ S, context, type: schemaType })
   );
 };
 
-const filteredDocumentListItems = (
-  S: StructureBuilder,
-  { schema }: StructureContext
-) => {
+const filteredDocumentListItems = (config: SingletonPluginListItemsConfig) => {
+  if (!config.S || !config.context) {
+    throw new Error(`
+      S and context must be provided
+      Ex: filteredDocumentListItems({ S, context })
+    `);
+  }
+  const { S, context } = config;
+  const { schema } = context;
+
   const singletons = getSingletonDocuments(schema);
 
   return S.documentTypeListItems().filter(
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,19 @@
-import { DocumentOptions } from 'sanity';
+import { ConfigContext, DocumentOptions } from 'sanity';
 import { StructureBuilder } from 'sanity/structure';
 import { ComponentType } from 'react';
 
 export interface SingletonDocumentListItemConfig {
   S: StructureBuilder;
-  schemaType: string;
+  context: ConfigContext;
+  type: string;
   title?: string;
   id?: string;
   icon?: ComponentType;
+}
+
+export interface SingletonPluginListItemsConfig {
+  S: StructureBuilder;
+  context: ConfigContext;
 }
 
 export interface SingletonPluginOptions extends DocumentOptions {
